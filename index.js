4bd961598ae2e0852d9fbae51087a32916f30c22
Amar P <amar.p@mobantica.com>
2024-04-27 20:37:07 +0530

    chagnes for confidence for answers

--- index.js
+++ index.js
@@ -1,66 +1,95 @@
 const testFolder = "./images/";
 const imageFile = "image-url.json";
 const fs = require("fs");
-const api_key = "YOUR_API_KEY";
+const api_key = "AIzaSyAjcoaa1Bohwv1qaNwnLU8TLuCN0-v0go0";
 const reqUrl =
-  "https://content-vision.googleapis.com/v1/images:annotate?alt=json&key=" +
-  api_key;
+  "https://vision.googleapis.com/v1/images:annotate?alt=json&key=" + api_key;
 const axios = require("axios");
-console.time("Duration");
 let imagesUrls = [];
-// imagesUrls = Array.from({length: 100}, (_, i) => "https://saral-numeracy.s3.ap-south-1.amazonaws.com/tests/image"+(i+15)+".jpg");
+const util = {
+  getSortedCordinates:(arr)=>{
+    return arr.sort((a,b)=>{return a.y - b.y})
+  },
+  getSubString: (iInput, from, to) => {
+    try {
+      return iInput.split(from)[1].split(to)[0].trim();
+    } catch (error) {
+      console.log("Error code 101");
+      return " ";
+    }
+  },
+  requestObj : {
+    features: [
+      {
+        maxResults: 50,
+        type: "LANDMARK_DETECTION",
+      },
+      {
+        maxResults: 50,
+        type: "FACE_DETECTION",
+      },
+      {
+        maxResults: 50,
+        model: "builtin/latest",
+        type: "OBJECT_LOCALIZATION",
+      },
+      {
+        maxResults: 50,
+        model: "builtin/latest",
+        type: "LOGO_DETECTION",
+      },
+      {
+        maxResults: 50,
+        type: "LABEL_DETECTION",
+      },
+      {
+        maxResults: 50,
+        model: "builtin/latest",
+        type: "DOCUMENT_TEXT_DETECTION",
+      },
+      {
+        maxResults: 50,
+        type: "SAFE_SEARCH_DETECTION",
+      },
+      {
+        maxResults: 50,
+        type: "IMAGE_PROPERTIES",
+      },
+      {
+        maxResults: 50,
+        type: "CROP_HINTS",
+      },
+    ],
+    imageContext: {
+      cropHintsParams: {
+        aspectRatios: [0.8, 1, 1.2],
+      },
+    },
+  }
+};
 
-const prepareRequest = (flag) => {
-  // for local images , images should not be more than 20MB
-  if (flag)
-    fs.readdir(testFolder, (err, files) => {
-      let arr = [];
-      files.forEach((file) => {
-        let bitmap = fs.readFileSync(testFolder + file);
-        let reqObj = Object.assign(
-          { features: [{ type: "TEXT_DETECTION" }], image: {} },
-          { image: { content: Buffer.from(bitmap).toString("base64") } }
-        );
-        arr.push(reqObj);
-      });
-      sendRequst(arr);
-    });
-
-  // using image urls.
-  if (!flag) {
+const prepareRequest = () => {
     fs.readFile(imageFile, "utf8", function (err, data) {
       if (err) throw err;
       imagesUrls = JSON.parse(data);
-      console.log("Total Images : ",imagesUrls.length);
+      console.log("Total Images : ", imagesUrls.length);
       let promises = [];
       while (imagesUrls.length) {
         let arr = [];
         let iArr = imagesUrls.splice(0, 15);
         for (let i = 0; i < iArr.length; i++) {
-          let reqObj = {
-            features: [{ type: "TEXT_DETECTION" }],
-            image: { source: { imageUri: iArr[i] } },
-          };
-          arr.push(reqObj);
+          arr.push({...(util.requestObj), image: { source: { imageUri: iArr[i] } }});
         }
         promises.push(sendRequst(arr));
       }
       Promise.all(promises).then(async (responses) => {
         let res = responses.flat(1);
-        //console.log("Total Records : ", imagesUrls.length);
-        console.log("Records saved  : ", res.length);
+        console.log(res[0].answers);
+        console.log("Records received  : ", res.length);
         console.timeEnd("Duration");
-        fs.writeFile(
-          new Date().toISOString() + "_data.json",
-          JSON.stringify(res),
-          "utf8",
-          () => {
-            console.log("Response received and mapped successfully..");
-          }
-        );
+        //saveDataAs("FinalData_", res);
       });
     });
-  }
 };
 
 const sendRequst = (reqBody) => {
@@ -78,64 +107,112 @@ const sendRequst = (reqBody) => {
 };
 
 const prepareData = (data) => {
+  console.log("preparing data...");
   let newData = data
     .map((response) => {
       try {
-        let newResponse = response.textAnnotations.sort(function (a, b) {
-          return a.boundingPoly.vertices[0].y - b.boundingPoly.vertices[0].y;
-        });
-        if (newResponse.length)
-          newResponse[0].property =
-            response.fullTextAnnotation.pages[0].property;
-        return newResponse;
+        if (response?.fullTextAnnotation?.pages[0]?.blocks.length){
+          return getArrengedBlocks(response.fullTextAnnotation.pages[0].blocks);
+        }else console.log("Plese check response .....",response);
+        return [];
       } catch (error) {
-        console.log(response);
+        console.log(error);
       }
     })
     .filter(function (el) {
       return el != null;
     });
-
-  const finalData = newData.map((iData) => {
-    let input = iData[0].description.replace(/\n/g, " ");
-    const util = {
-      getSubString: (iInput, from, to) => {
-        try {
-          return iInput.split(from)[1].split(to)[0].trim();
-        } catch (error) {
-          onsole.log("start : ", from);
-          onsole.log("end : ", to);
-          console.log("input : ", input);
-          return " ";
-        }
-      },
-      getAnswers: (answerString) => {
-        let iAns = [];
-        for (let index = 1; index < 13; index++) {
-          let str = (answerString.split(index+")")[1].trim()).split(" ")[0];
-          iAns.push(index+") "+(str.includes(")")?"":str));
-        }
-        return iAns;
-      },
-    };
-    let response = Object.assign(
-      {},
-      { property: iData[0].property.detectedLanguages }
-    );
-    try {
-      response.title = input.split("Name:")[0].trim();
-      response.name = util.getSubString(input, "Name:", "School:");
-      response.class = util.getSubString(input, "Class:", "Start time:");
-      response.date = util.getSubString(input, "Date:", "End Time:");
-      response.school = util.getSubString(input, "School:", "1)");
-      response.startTime = util.getSubString(input, "Start time:", "Date:");
-      response.endTime = util.getSubString(input, "End Time:", "3)");
-      response.answers = util.getAnswers(util.getSubString(input, "Final Answers:", "Add"));
-      response.sheetCode = "Add_" + util.getSubString(input, "Add_", "1/");
-    } catch (error) {}
-    return response;
-  });
-  return finalData;
+  return getFinalData(newData);
 };
 
+const getArrengedBlocks = (dataArr)=>{
+  console.log("preparing blocks ...");
+  let blocks = dataArr.map((data)=>{
+    let text = data.paragraphs[0].words.map((word)=>{
+      return word.symbols.map((symbol)=>{return symbol?.property?.detectedBreak?.type=="SPACE"? symbol.text+" ": symbol.text}).join("")
+    }).join(" ")
+      return {...data.paragraphs[0],words:text};
+   });
+  let arr = [];
+  while(blocks.length){
+    const min = util.getSortedCordinates(blocks[0].boundingBox.vertices)[0].y - 50;
+    const max = util.getSortedCordinates(blocks[0].boundingBox.vertices)[3].y - 10;
+    const match = blocks.filter((block)=>{
+      return util.getSortedCordinates(block.boundingBox.vertices)[0].y >= min &&  util.getSortedCordinates(block.boundingBox.vertices)[0].y <= max;
+    }).sort((a,b)=>{
+        return a.boundingBox.vertices[0].x - b.boundingBox.vertices[0].x;
+    });
+    arr.push(match);
+    blocks = Array.from(blocks.filter((block)=>{
+      return !(util.getSortedCordinates(block.boundingBox.vertices)[0].y >= min &&  util.getSortedCordinates(block.boundingBox.vertices)[0].y <= max);
+  }));
+  if(match.length==0){
+    console.log("Matching blockes not found")
+    break
+  };
+  }
+return arr;
+}
+
+const getFinalData = (newData)=>{
+  console.log("preparing final data ...")
+  return newData.map((iData) => {
+    let response = {};
+    try {
+      let rowArr = iData.map((rows)=>{
+        return rows.map((row)=>{ return row.words; }).join(" ");
+      });
+      let dataString = rowArr.filter((row)=>{return row.includes("Name")})[0];
+      response.name = util.getSubString(dataString,":","Class");
+      response.class = util.getSubString(dataString,"Class :","Date :");
+      response.date = util.getSubString(dataString,"Date :",".");
+      dataString = rowArr.filter((row)=>{return row.includes("School")})[0];
+      response.school = util.getSubString(dataString,"School :","Start");
+      response.startTime = util.getSubString(dataString,"time :","End");
+      response.endTime = util.getSubString(dataString,"Time :",".");
+      let iIndex = rowArr.findIndex(element => element.includes("Final"));
+      response.answers = [];
+      for (let index = iIndex + 1; index < iData.length - 1 ; index++) {
+        if(!(JSON.stringify(iData[index])).includes(")"))break;
+        console.log(rowArr[index]);
+        let ansArr = [];
+        for (let i = 0; i < iData[index].length; i++) {
+            let question = 0;
+            let answer = "";let confidence = 0;
+            let arr = iData[index][i].words.split(")");
+            if(arr[1]&&(arr[1].trim()).length){
+                question = parseInt(arr[0].replace(/\D/g,''));
+                answer = arr[1].trim();
+                confidence = iData[index][i].confidence;
+            }else{
+              question = parseInt(iData[index][i].words.replace(/\D/g,''));
+              if(iData[index][i+1] && !iData[index][i+1].words.includes(")")){
+                  answer = iData[index][i+1].words;
+                  confidence = iData[index][i+1].confidence;
+                  i++;
+              }
+            }
+            
+            ansArr.push({question,answer,confidence});
+        }
+        response.answers.push(...ansArr);
+      }
+    } catch (error) {
+      console.log("Error code 102",error);
+    }
+    return response;
+  });
+}
+
+const saveDataAs = (name, data) => {
+  fs.writeFile(
+    testFolder+ name + new Date().toISOString() + "_Data.json",
+    JSON.stringify(data),
+    "utf8",
+    () => {
+      console.log(name + " file saved successfully.");
+    }
+  );
+};
+console.time("Duration");
 prepareRequest();
